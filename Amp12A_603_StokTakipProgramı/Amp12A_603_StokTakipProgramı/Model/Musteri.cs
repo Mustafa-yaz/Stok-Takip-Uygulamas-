9a244c62cf96452e4d5b569d539dacf39b7ebcb3
Mustafa <yazicioglumustafa86@gmail.com>
2024-12-09 12:39:43 +0300

    Add files via upload

--- Amp12A_603_StokTakipProgramı/Amp12A_603_StokTakipProgramı/Model/Musteri.cs
+++ Amp12A_603_StokTakipProgramı/Amp12A_603_StokTakipProgramı/Model/Musteri.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,20 @@ namespace Amp12A_603_StokTakipProgramı.Model
 {
     public class Musteri
     {
+        [Key]
+
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+        public string FirmaAdi { get; set; }
+        public string Telefon { get; set; }
+        public string Mail { get; set; }
+        public string Adres { get; set; }
+
+
+
+        public virtual ICollection<Satis> Satıslar { get; set;}=new HashSet<Satis>();
+
+
 
     }
 }
---
+++ Amp12A_603_StokTakipProgramı/Amp12A_603_StokTakipProgramı/Model/Satis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amp12A_603_StokTakipProgramı.Model
+{
+    public class Satis
+    {
+
+    }
+}
